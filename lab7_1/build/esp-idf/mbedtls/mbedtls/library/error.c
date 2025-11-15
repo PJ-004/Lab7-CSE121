6346c33d31e696dea7794a3d7170e1cfd68957e2
Pranav Jha <114767771+PJ-004@users.noreply.github.com>
2025-11-14 16:00:03 -0800

    Delete lab7_1/build directory

--- lab7_1/build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/home/pj/Documents/cse121/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
--- lab7_1/build/esp-idf/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
+++
@@ -1,1 +0,0 @@
-/home/pj/Documents/cse121/esp/esp-idf/components/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
